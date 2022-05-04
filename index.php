dd16034b77a5a53f405d7153532c49cf1332f6c8
sarlay <dev@sarlays.com>
2022-05-04 15:19:30 +0200

    updated messages and style
    added a header, messages list and new message form

--- index.php
+++ index.php
@@ -1,27 +1,46 @@
-
 <!DOCTYPE HTML>
-<link rel="stylesheet" href="truc.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="stylesheet.css" type="text/css" media="screen" />
 <html>
     <head>
         <title>
-NoName
+        NoName Chat
         </title>
     </head>
-    <body>
-<?php
-$user = "sarlay";
-$password = getenv("MY_SQL_PASSWORD"); #REPLACE getenv("") with your password
-$database = "messages";
-$table = "messages";
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  echo "<h2>Messages:</h2><ol>";
-  foreach($db->query("SELECT * FROM $table") as $row) {
-      
-    echo "<li>" . $row['content'] . "</li>", "<text> écrit par: " . $row['username'] . "</text>";
 
-  }
-  echo "</ol>";
-?>
+
+
+    <body>
+
+        <header class="header">
+         
+                <a class="website_title">N/A Chat</a>
+
+        </header>
+    <div class="messages_list">
+      <h2>Messages:</h2>
+      <?php
+      $user = "sarlay";
+      $password = "password"; #REPLACE getenv("") with your password
+      $database = "messages";
+      $table = "messages";
+        $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+        echo "<ol>";
+        foreach($db->query("SELECT * FROM $table") as $row) {
+            
+          echo "<li>" . $row['content'] .  "<text class='username'>" . $row['username'] . "</text>", "</li>";
+
+        }
+        echo "</ol>";
+      ?>
+    </div>
+    <div>
+    <h2>Add a new message:</h2>
+        <form method="post">
+          <input type="text" name="username">
+          <input type="text" name="content">
+          <button type="submit">Envoyer</button>
+        </form>
+    </div>
     </body>
 </html>
 

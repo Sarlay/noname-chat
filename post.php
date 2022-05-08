d63cf2c025da84c507ffe8fcbb8f8b09a0c6121d
Sarlay <60151189+Sarlay@users.noreply.github.com>
2022-05-08 10:22:37 +0000

    autorise les espaces

--- post.php
+++ post.php
@@ -9,8 +9,8 @@ $db = new PDO("mysql:host=$server;dbname=$database", $user, $password);
 
 
 # only allow letters and numbers
-$message_username=preg_replace("/[^a-zA-Z0-9]+/", "", $_POST["username"]); # Prevents sql injection through regex
-$message_content=preg_replace("/[^a-zA-Z0-9]+/", "", $_POST["content"]); # Prevents sql injection through regex
+$message_username=preg_replace("/[^a-zA-Z0-9 ]+/", "", $_POST["username"]); # Prevents sql injection through regex
+$message_content=preg_replace("/[^a-zA-Z0-9 ]+/", "", $_POST["content"]); # Prevents sql injection through regex
 
 
 $exec = $db->prepare("INSERT INTO messages (content, username) VALUES(?,?)");

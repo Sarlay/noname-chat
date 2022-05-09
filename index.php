775e193dc98c20fa6472aa813df70e8b8f508b50
Sarlay <60151189+Sarlay@users.noreply.github.com>
2022-05-09 12:31:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,33 +18,15 @@
         </header>
     <div class="messages_list">
       <h2>Messages:</h2>
-      <?php
-      ini_set('display_errors', 1);
-      error_reporting(E_ALL);
-      $user = "epiz_31661084";
-      $server = "sql109.epizy.com";
-      $password = ""; #REPLACE getenv("") with your password
-      $database = "epiz_31661084_noname";
-      $table = "messages";
-        $db = new PDO("mysql:host=$server;dbname=$database", $user, $password);
-        echo "<ol>";
-        foreach($db->query("SELECT * FROM $table") as $row) { 
-          echo "<li><text class='username'>" . $row['username'] . "</text></li>".  "<text class='message_line'>" . $row['content'] . "</text>";
-
-        }
-        echo "</ol>";
-      ?>
+      <iframe name="post_iframe" id="post_iframe"></iframe>
     </div>
     <div>
     <h2>Add a new message:</h2>
         <form target="post_iframe" action="post.php" method="post">
           <input type="text" name="username" placeholder="Nom d'utilisateur"><br/><br/>
-          <input type="text" name="content" placeholder="Contenu du message"><br/><br/>
-          <button type="submit">Envoyer</button>
+          <div><input type="text" name="content" placeholder="Contenu du message"><button type="submit">Envoyer</button></div>
+          <input type="button" value="Refresh Page" onClick="refresh">
         </form>
-        <iframe style="" name="post_iframe" id="post_iframe"></iframe>
     </div>
     </body>
 </html>
-
-

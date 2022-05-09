853370f5e667b51c0e2095f984b10a26ae6058e3
Sarlay <60151189+Sarlay@users.noreply.github.com>
2022-05-09 12:32:03 +0200

    Create list.php

---
+++ list.php
@@ -0,0 +1,16 @@
+<?php
+  ini_set('display_errors', 1);
+  error_reporting(E_ALL);
+  $user = "epiz_31661084";
+  $server = "sql109.epizy.com";
+  $password = "";
+  $database = "epiz_31661084_noname";
+  $table = "messages";
+    $db = new PDO("mysql:host=$server;dbname=$database", $user, $password);
+    echo "<ol>";
+    foreach($db->query("SELECT * FROM $table") as $row) { 
+      echo "<li><text class='username'>" . $row['username'] . "</text></li>".  "<text class='message_line'>" . $row['content'] . "</text>";
+
+    }
+    echo "</ol>";
+?>

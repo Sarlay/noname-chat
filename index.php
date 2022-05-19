0034cd1b5ec30a86fc7f4c7c5f8cadd5b2375768
raphael <spam@sarlays.com>
2022-05-19 12:03:51 +0200

    changed styling

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
     <body>
 
         <header class="header">
-         
+
                 <a class="website_title">N/A Chat</a>
 
         </header>
     <div class="messages_list">
       <h2>Messages:</h2>
       <iframe name="post_iframe" id="post_iframe" src="list.php">
-      <?php 
+      <?php
       include 'list.php';
       ?>
       </iframe>
@@ -27,8 +27,8 @@
     <div>
     <h2>Envoyer un message:</h2>
         <form target="post_iframe" action="post.php" method="post">
-          <input type="text" name="username" placeholder="Nom d'utilisateur"><br/><br/>
-          <div><input type="text" class="input_content" name="content" placeholder="Contenu du message"></input><input class="hide_text" type="submit"></input></div>
+          <input type="text" required="required" name="username" placeholder="Nom d'utilisateur"><br/><br/>
+          <div><input type="text" required="required" class="input_content" name="content" placeholder="Contenu du message"></input><input class="hide_text" type="submit"></input></div>
         </form>
     </div>
     </body>

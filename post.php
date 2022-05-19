4b88df3d8fc598e920994dd53e494634ff7ebb0c
Sarlay <60151189+Sarlay@users.noreply.github.com>
2022-05-19 08:54:38 +0000

    Update post.php

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="stylesheet.css"/>
  <?php
 $user = "epiz_31661084";
-$password = "";
+$password = "nonameNSI2";
 $database = "epiz_31661084_noname";
 $table = "messages";
 $server = "sql109.epizy.com";
@@ -13,12 +13,13 @@ $filter = "/[^a-zA-Z0-9ç?!. ]+/";
 $message_username=preg_replace($filter, "", $_POST["username"]); # Prevents sql injection through regex
 $message_content=preg_replace($filter, "", $_POST["content"]); # Prevents sql injection through regex
 
+if(isset($_POST['username']) && isset($_POST['content'])) {
+    $exec = $db->prepare("INSERT INTO messages (content, username) VALUES(?,?)");
+    $exec->execute(array($message_content,$message_username));
 
-$exec = $db->prepare("INSERT INTO messages (content, username) VALUES(?,?)");
-$exec->execute(array($message_content,$message_username));
-
-if($exec){
-    include 'list.php';
-}else{
-    echo "Ca beug";
+    if($exec){
+        include 'list.php';
+    } else {
+        echo "Ca beug";
+    }
 }

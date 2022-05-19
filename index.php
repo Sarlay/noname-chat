a76a9c74bb6f85d2129e85b777dd65bc1d4173fc
Sarlay <60151189+Sarlay@users.noreply.github.com>
2022-05-19 08:52:39 +0000

    Update index.php

--- index.php
+++ index.php
@@ -18,13 +18,17 @@
         </header>
     <div class="messages_list">
       <h2>Messages:</h2>
-      <iframe name="post_iframe" id="post_iframe"></iframe>
+      <iframe name="post_iframe" id="post_iframe" src="list.php">
+      <?php 
+      include 'list.php';
+      ?>
+      </iframe>
     </div>
     <div>
     <h2>Envoyer un message:</h2>
         <form target="post_iframe" action="post.php" method="post">
           <input type="text" name="username" placeholder="Nom d'utilisateur"><br/><br/>
-            <div><input class="input_content" type="text" name="content" placeholder="Contenu du message"></input><input class="hide_text" type="submit">Envoyer</input></div>
+          <div><input type="text" class="input_content" name="content" placeholder="Contenu du message"></input><input class="hide_text" type="submit"></input></div>
         </form>
     </div>
     </body>
